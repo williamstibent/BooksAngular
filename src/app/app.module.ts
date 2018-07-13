8ac4735839f2d24005a1561aefba8fa09b3aee85
Juan Camilo Colmenares <jc.colemenares@uniandes.edu.co>
2018-07-12 19:49:48 -0500

    Header and nav angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopNavAppComponent } from './top-nav-app/top-nav-app.component';
+import { VerticalNavAppComponent } from './vertical-nav-app/vertical-nav-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavAppComponent,
+    VerticalNavAppComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/top-nav-app/top-nav-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'top-nav-app',
+  templateUrl: './top-nav-app.component.html',
+  styleUrls: ['./top-nav-app.component.css']
+})
+export class TopNavAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
